6fa029ae819bb723177f01283c434d4bc2918f40
zenbrito <zeniltonsoares@gmail.com>
2022-02-21 23:08:48 -0300

    Aula 01 de JavaScript: Primeiros comandos

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*Nessa primeira aula de JS, o professor Rafael apresentou os primeiros comandos, como os descritos abaixo:
+
+1º comando: ALERT - Exibe uma mensagem de alerta para o usuário, ao acessar o navegador;
+
+2º comando: VAR (variável) - pois ao usar esse comando, também podemos distinguir o tipo de dado a ser usado, se vai ser uma STRING ou um número INTEIRO;
+
+3º comando: CONSOLE.LOG - Para imprimir na tela (console), a ação do comando inserido no JS;
+
+4º comando: .REPLACE("...", "...") - usado para substituir um dado por outro, como no exemplo abaixo, alteramos Japão para Brasil.
+*/
+
+var nome = "Zeni Brito";
+var idade = "40";
+var idade2 = "10";
+var frase = "Japão é o melhor time do mundo";
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
